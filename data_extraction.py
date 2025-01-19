e0b185d3a15ba7ee0ec3cb5afc3ffb95a37e6fec
JordanNikomedes1 <goldfincharchitect@mail.com>
2025-01-19 21:00:37 +0000

    Added a docstring in the read_rds_table method within the dataextraction class.

--- data_extraction.py
+++ data_extraction.py
@@ -6,6 +6,16 @@ class DataExtractor:
         pass
 
     def read_rds_table(self, engine, table_name):
+        """This method reads the table from the database, 
+        then converts it into a pandas dataframe so it can be manipulated easier within python.
+        
+        Args:
+            engine(obj): parses the engine to connect with the database.
+            table_name(str): converts the targeted table into a dataframe.
+        
+        Return:
+            Returns a dataframe using the pandas library.
+        """
 
         with engine.begin() as conn:
             return pd.read_sql_table(table_name, con=conn)
