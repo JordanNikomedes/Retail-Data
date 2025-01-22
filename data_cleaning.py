1a72b3e155234a72e0127d1ee0f15ba21ad56c21
JordanNikomedes1 <goldfincharchitect@mail.com>
2025-01-22 19:24:55 +0000

    Added docstring to the newly created method, clean_user_data.

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,6 +7,14 @@ class DataCleaning:
         pass
 
     def clean_user_data(self, df):
+        '''This method cleans the user data to make sure there are no anomilies within the table.
+        
+        Args:
+            df(table): Takes the pandas dataframe as the parameter.
+
+        Return:
+            Returns the legacy users cleaned with no anomilies.
+        '''
         df = df.replace(['null', 'NULL', 'NaN', 'None'], pd.NA)
         df = df.dropna(inplace= True)
         df['join_date'] = pd.to_datetime(df['join_date'], errors= 'coerce')
