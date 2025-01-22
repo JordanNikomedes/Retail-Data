e745fb860e0f81ec734f8bad2c716e5e94a8b2fa
JordanNikomedes1 <goldfincharchitect@mail.com>
2025-01-22 18:58:29 +0000

    Added clean user data method to the data_cleaning class.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,13 @@
+import pandas as pd
+
+
 class DataCleaning:
     
     def __init__(self):
         pass
 
     def clean_user_data(self, df):
-        df = self.clean_invalid_date(df)
+        df = df.replace(['null', 'NULL', 'NaN', 'None'], pd.NA)
+        df = df.dropna(inplace= True)
+        df['join_date'] = pd.to_datetime(df['join_date'], errors= 'coerce')
+        df['join_date'] = df['join_date'].fillna(pd.Timestamp('1000-01-01'))
